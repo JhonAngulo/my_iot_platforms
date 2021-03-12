d7b9738742b82b84fe73ac50aab3cc637cf46a21
Jhon Angulo <jhonmanuelm89@gmail.com>
2021-03-11 22:30:40 -0500

    add broker

--- mqtt_server/server.js
+++ mqtt_server/server.js
@@ -1,1 +1,57 @@
 'use strict'
+
+require('dotenv').config()
+
+// const debug = require('debug')('mqtt_server:mqtt')
+const chalk = require('chalk')
+const aedes = require('aedes')
+const redisMqEmitter = require('mqemitter-redis')
+const redisPersistence = require('aedes-persistence-redis')
+// const storage = require('')
+const port = 1883
+
+const aedesPersistenceRedis = redisPersistence({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_URL,
+  family: 4
+})
+
+const mq = redisMqEmitter({
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_URL,
+  family: 4
+})
+
+const broker = aedes.Server({
+  persistence: aedesPersistenceRedis, mq
+})
+
+const server = require('net').createServer(broker.handle)
+
+broker.on('client', (client) => {
+  console.log(`Client Connected ${client.id}`)
+})
+
+broker.on('clientDisconnect', (client) => {
+  console.log(`Client Disconnected ${client.id}`)
+})
+
+broker.on('publish', (packet, _client) => {
+  console.log('Received', packet.topic)
+  console.log('Payload', packet.payload)
+})
+
+broker.on('connectionError', handleFatalError)
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[mqtt_server-mqtt]')} server is running`)
+})
